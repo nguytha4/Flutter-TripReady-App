e3a5ee5aa376ff17a4bf53e9ecd9943c0e6a7137
Granit23 <granitarifi.23@gmail.com>
2020-05-01 10:19:20 -0400

    adds styling to login page as well as authentication

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,12 +5,17 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:capstone/widgets/capstone_scaffold.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+
+
 
 // import 'package:google_sign_in/google_sign_in.dart';
 // import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
-// final GoogleSignIn _googleSignIn = GoogleSignIn();
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 class LoginScreen extends StatefulWidget {
   static const routeName = 'login_screen';
@@ -25,6 +30,7 @@ class LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
         actions: <Widget>[
           Builder(builder: (BuildContext context) {
             return FlatButton(
@@ -56,7 +62,7 @@ class LoginScreenState extends State<LoginScreen> {
             _EmailPasswordForm(),
             // _EmailLinkSignInSection(),
             // _AnonymouslySignInSection(),
-            // _GoogleSignInSection(),
+            _GoogleSignInSection(),
             // _PhoneSignInSection(Scaffold.of(context)),
             // _OtherProvidersSignInSection(),
           ],
@@ -89,43 +95,26 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          logo(),
+          buildEmailBox(),
+          buildPasswordBox(),
           Container(
-            child: const Text('Sign in with email and password'),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(labelText: 'Email'),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _passwordController,
-            decoration: const InputDecoration(labelText: 'Password'),
-            validator: (String value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            alignment: Alignment.center,
-            child: RaisedButton(
-              onPressed: () async {
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+              alignment: Alignment.center,
+              child: ButtonTheme(
+                minWidth: double.infinity,
+                child:RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0)
+                ),
+                onPressed: () async {
                 if (_formKey.currentState.validate()) {
                   _signInWithEmailAndPassword(context);
                 }
               },
-              child: const Text('Submit'),
-            ),
-          ),
+                child: const Text('Submit', style: TextStyle(color: Colors.white)),
+              ),
+            )),
           Container(
             alignment: Alignment.center,
             padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -142,72 +131,95 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
       ),
     );
   }
-// class _EmailPasswordFormState extends State<_EmailPasswordForm> {
-//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-//   bool _success;
-//   String _userEmail;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//       key: _formKey,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           Container(
-//             child: const Text('Test sign in with email and password'),
-//             padding: const EdgeInsets.all(16),
-//             alignment: Alignment.center,
-//           ),
-//           TextFormField(
-//             controller: _emailController,
-//             decoration: const InputDecoration(labelText: 'Email'),
-//             validator: (String value) {
-//               if (value.isEmpty) {
-//                 return 'Please enter some text';
-//               }
-//               return null;
-//             },
-//           ),
-//           TextFormField(
-//             controller: _passwordController,
-//             decoration: const InputDecoration(labelText: 'Password'),
-//             validator: (String value) {
-//               if (value.isEmpty) {
-//                 return 'Please enter some text';
-//               }
-//               return null;
-//             },
-//           ),
-//           Container(
-//             padding: const EdgeInsets.symmetric(vertical: 16.0),
-//             alignment: Alignment.center,
-//             child: RaisedButton(
-//               onPressed: () async {
-//                 if (_formKey.currentState.validate()) {
-//                   _signInWithEmailAndPassword(context);
-//                 }
-//               },
-//               child: const Text('Submit'),
-//             ),
-//           ),
-//           Container(
-//             alignment: Alignment.center,
-//             padding: const EdgeInsets.symmetric(horizontal: 16),
-//             child: Text(
-//               _success == null
-//                   ? ''
-//                   : (_success
-//                       ? ''
-//                       : 'Sign in failed'),
-//               style: TextStyle(color: Colors.red),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
+
+  Widget logo() {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+      height: 200,
+      width: 200,
+      child: Image.asset('assets/images/logo.png')
+    )
+    ); 
+  }
+  Align buildEmailBox() {
+    return Align(
+          alignment: Alignment.center,
+          child:Container(
+          padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+          child:TextFormField(
+            style: TextStyle(fontSize: 13),
+            controller: _emailController,
+            decoration: new InputDecoration(
+                      labelText: "Email",
+                      fillColor: Colors.white,
+                      border: new OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(30.0),
+                        borderSide: new BorderSide(
+                          color: Colors.blue,
+                          width: 2
+                        ),
+                      ),
+                      prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: IconTheme(
+                      data: IconThemeData(color: Colors.blue),
+                      child: Icon(Icons.email)
+                    )
+                  )
+                    //fillColor: Colors.green
+                      //fillColor: Colors.green
+                    ),
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          )));
+  }
+
+  Widget buildPasswordBox() {
+    return Align(
+        alignment: Alignment.center,
+        child:Container(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+          child:TextFormField(
+            autofocus: true,
+            obscureText: true,
+            style: TextStyle(fontSize: 13),
+            controller: _passwordController,
+            decoration: new InputDecoration(
+                    // hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
+                    // hintText: "PASSWORD",
+                    labelText: "Password",
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 10
+                      ),
+                    ),
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: IconTheme(
+                      data: IconThemeData(color: Colors.blue),
+                      child: Icon(Icons.lock)
+                    )
+                  )
+                    //fillColor: Colors.green
+                  ),
+                  
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter a password';
+              }
+              return null;
+            },
+          ))
+    );
+  }
 
   Widget _entryField(String title, {bool isPassword = false}) {
     return Container(
@@ -265,5 +277,78 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
       });
     }
     }
+  }
+}
+
+class _GoogleSignInSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _GoogleSignInSectionState();
+}
+
+class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
+  bool _success;
+  String _userID;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            
+            color: Colors.red,
+            onPressed: () async {
+              _signInWithGoogle();
+            },
+            child: const Text('Sign in with Google', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            _success == null
+                ? ''
+                : (_success
+                    ? 'Successfully signed in, uid: ' + _userID
+                    : 'Sign in failed'),
+            style: TextStyle(color: Colors.red),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Example code of how to sign in with google.
+  void _signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+      } else {
+        _success = false;
+      }
+    });
   }
 }
