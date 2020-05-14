3c0fc7b55c612630cb206e04dba7d9438eaea114
Granit23 <granitarifi.23@gmail.com>
2020-05-14 15:22:05 -0400

    changes login screen route

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -30,31 +30,7 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return CapstoneScaffold(
         title: widget.title,
-        hideDrawer: true,
-        // actions: <Widget>[
-        //   Builder(builder: (BuildContext context) {
-        //     return FlatButton(
-        //       child: const Text('Sign out'),
-        //       textColor: Colors.blue,
-        //       onPressed: () async {
-        //         final FirebaseUser user = await _auth.currentUser();
-        //         if (user == null) {
-        //           Scaffold.of(context).showSnackBar(const SnackBar(
-        //             content: Text('No one has signed in.'),
-        //           ));
-        //           return;
-        //         }
-        //         _signOut();
-
-        //         final String uid = user.uid;
-        //         Scaffold.of(context).showSnackBar(SnackBar(
-        //           content: Text(uid + ' has successfully signed out.'),
-        //         ));
-        //       },
-        //     );
-        //   })
-        // ],
-      
+        hideDrawer: true, 
       child: Builder(builder: (BuildContext context) {
         return ListView(
           scrollDirection: Axis.vertical,
@@ -219,7 +195,6 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
     super.dispose();
   }
 
-  // Example code of how to sign in with email and password.
   void _signInWithEmailAndPassword(BuildContext context) async {
     FirebaseUser user;
     try {
@@ -232,7 +207,7 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
       print(err.toString());
     } 
     finally {
-    
+
     if (user != null) {
       setState(() {
         _success = true;
@@ -246,80 +221,5 @@ class _EmailPasswordFormState extends State<_EmailPasswordForm> {
       });
     }
     }
-  }
-}
-
-class _GoogleSignInSection extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _GoogleSignInSectionState();
-}
-
-class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
-  bool _success;
-  String _userID;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(16),
-          alignment: Alignment.center,
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          alignment: Alignment.center,
-          child: RaisedButton(
-            
-            color: Colors.red,
-            onPressed: () async {
-              _signInWithGoogle();
-            },
-            child: const Text('Sign in with Google', style: TextStyle(color: Colors.white)),
-          ),
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text(
-            _success == null
-                ? ''
-                : (_success
-                    ? 'Successfully signed in, uid: ' + _userID
-                    : 'Sign in failed'),
-            style: TextStyle(color: Colors.red),
-          ),
-        )
-      ],
-    );
-  }
-
-  // Example code of how to sign in with google.
-  void _signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    final FirebaseUser user =
-        (await _auth.signInWithCredential(credential)).user;
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-    setState(() {
-      if (user != null) {
-        _success = true;
-        _userID = user.uid;
-        Navigator.of(context).pop();
-        Navigator.of(context).popAndPushNamed(ChecklistScreen.routeName);
-      } else {
-        _success = false;
-      }
-    });
   }
 }
