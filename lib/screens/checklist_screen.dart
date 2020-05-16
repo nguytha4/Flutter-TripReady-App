bda6070b3a97cb8b8ad031d3dc68375c2e9df703
Granit23 <granitarifi.23@gmail.com>
2020-05-16 19:29:37 -0400

    adds crowd-sourcing sort of

--- lib/screens/checklist_screen.dart
+++ lib/screens/checklist_screen.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:capstone/tripready.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'dart:developer';
+import 'dart:collection';
 
-class ChecklistScreen extends StatefulWidget {
+class ChecklistScreen extends StatefulWidget  {
   static const routeName = 'checklist_screen';
+  final Destination destination;
+
+  ChecklistScreen({this.destination});
   @override
   _ChecklistScreenState createState() => _ChecklistScreenState();
 }
 
-class _ChecklistScreenState extends State<ChecklistScreen> {
+class _ChecklistScreenState extends State<ChecklistScreen> with TickerProviderStateMixin{
+  var _counter = 0;
   String userId;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
@@ -19,7 +26,6 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
     getUser();
   }
 
-  
   Widget build(BuildContext context) {
     return CapstoneScaffold(
       title: "Checklist",
@@ -50,7 +56,7 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
             onDismissed: (direction) {
               setState(() {
                 _data.removeAt(index);
-                Firestore.instance.collection('users').document(userId).collection('checklist').document(_id).updateData({
+                Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').document(_id).updateData({
                     'children': _data
                   });  
               });
@@ -63,10 +69,9 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
                 child:Checkbox(
               value: _data[index]['checked'] ?? false,
               onChanged: (bool value) {
-                print(userId);
                 setState(() {
                   _data[index]['checked'] = value;
-                  Firestore.instance.collection('users').document(userId).collection('checklist').document(_id).updateData({
+                  Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').document(_id).updateData({
                     'children': _data,
                   });  
                 });
@@ -85,12 +90,9 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
 
   Widget buildPanels() {
        return StreamBuilder(
-          stream: Firestore.instance.collection('users').document(userId).collection('checklist').snapshots(),
+          stream: Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').snapshots(),
           builder: (content, snapshot) {
-            print(snapshot.data.documents);
             if (snapshot.hasData && snapshot.data.documents.length != 0) {
-               
-               print(snapshot.data.documents.length);
                return ListView.builder(
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
@@ -101,11 +103,11 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
               return 
               GestureDetector(
               onLongPress: () {
-        createDeleteDialog(context).then((val) {;
+        createDeleteDialog(context).then((val) {
           if (val != null) {
           setState(() {
-            Firestore.instance.collection('users').document(userId).collection('checklist').document(_id).delete();
-            // _data.add(Entry(val));
+            Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').document(_id).delete();
+            _removeFromCrowdSource(_data['title']);
           });
         }});
       },
@@ -114,8 +116,8 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
                 value: _data['checked'] ?? false,
                 onChanged: (bool value) {
                   setState(() {
-                    // print(_data);
-                  Firestore.instance.collection('users').document(userId).collection('checklist').document(_id).updateData({
+                    
+                  Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').document(_id).updateData({
                     'checked': value,
                   });                    
                   });
@@ -137,7 +139,7 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
                       };
                       var _data2 = List.from(_data['children']);
                       _data2.add(c);
-                      Firestore.instance.collection('users').document(userId).collection('checklist').document(_id).updateData({
+                      Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').document(_id).updateData({
                       'children': _data2
                       }); 
                     });
@@ -155,24 +157,135 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
             });
   }
   Widget fab() {
-    return FloatingActionButton(
-      child: Icon(Icons.add),
-      backgroundColor: Colors.blue,
-      onPressed: () {
-        createAlertDialog(context).then((val) {
-          if (val != null) {
-          setState(() {
-            Firestore.instance.collection('users').document(userId).collection('checklist').add({
-                      'title': val,
-                      'checked': false,
-                      'date': DateTime.now(),
-                      'children': []
+
+ return SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          animatedIconTheme: IconThemeData(size: 22),
+          backgroundColor: Color(0xFF2196F3),
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+                // FAB 1
+                SpeedDialChild(
+                child: Icon(Icons.add),
+                backgroundColor: Color(0xFF2196F3),
+                onTap: () {    
+                  
+                  createAlertDialog(context).then((val) {
+                  if (val != null) {
+                  setState(() {
+                    log(val);
+                    _addToCrowdSource(val);
+                    Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').add({
+                              'title': val,
+                              'checked': false,
+                              'date': DateTime.now(),
+                              'children': []
+                            });          
+                  });
+                }});
+                 },
+                label: 'Add Entry',
+                labelStyle: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 16.0),
+                labelBackgroundColor: Color(0xFF2196F3)),
+                // FAB 2
+                SpeedDialChild(
+                child: Icon(Icons.people),
+                backgroundColor: Color(0xFF2196F3),
+                onTap: () {
+                   setState(() {
+                     List user_checklist = [];
+                     var user_document=Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').getDocuments().then((QuerySnapshot snapshot) {
+                      snapshot.documents.forEach((element) {print('${element.data['title']}');
+                      user_checklist.add(element.data['title'].toLowerCase().trim());
+                      });
+                      _getCrowdSource(user_checklist);
                     });
-            // _data.add(Entry(val));
-          });
-        }});
-      },
+                      
+                   });
+                },
+                label: 'Crowd Source',
+                labelStyle: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 16.0),
+                labelBackgroundColor: Color(0xFF2196F3))
+          ],
+        );
+  }
+
+  void _addToCrowdSource(String val) {
+    var document = Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced')
+    .get().then((DocumentSnapshot) {
+      var global_checklist_entry = val.toLowerCase().trim();
+      var num_entry = DocumentSnapshot.data[global_checklist_entry];
+      if (num_entry == null) {
+        Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced').updateData({
+                    global_checklist_entry: 1        
+                            });
+      } else {
+        Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced').updateData({
+                    global_checklist_entry: num_entry + 1        
+                            });
+      }
+    }
     );
+   
+  }
+
+  void _removeFromCrowdSource(String val) {
+    var document = Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced')
+    .get().then((DocumentSnapshot) {
+      var global_checklist_entry = val.toLowerCase().trim();
+      var num_entry = DocumentSnapshot.data[global_checklist_entry];
+      log('name: $global_checklist_entry\n num_entry: $num_entry');
+      if (num_entry != null) {
+        if (num_entry - 1 == 0) {
+          Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced').updateData({
+                    global_checklist_entry: FieldValue.delete()     
+                            });
+      } else {
+        
+        Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced').updateData({
+                    global_checklist_entry: num_entry - 1        
+                            });
+      }
+    } 
+    }
+    );
+  }
+
+  void _getCrowdSource(List user_checklist) {
+    var document = Firestore.instance.collection('destination').document(this.widget.destination.documentID).collection('checklist_sourced').document('checklist_sourced')
+    .get().then((DocumentSnapshot) {
+      
+      List crowd_source;
+      
+      Map docData = DocumentSnapshot.data;
+      List mapKeys = DocumentSnapshot.data.keys.toList(growable: false);
+      List mapVals = DocumentSnapshot.data.values.toList();
+      // print(docData);
+      var sortedKeys = DocumentSnapshot.data.keys.toList(growable: false)..sort((k1, k2) => DocumentSnapshot.data[k1].compareTo(DocumentSnapshot.data[k2]));
+      if (sortedKeys.length < 5) {
+        crowd_source = List.from(sortedKeys.reversed).sublist(0,sortedKeys.length);
+      } else {
+        crowd_source = List.from(sortedKeys.reversed).sublist(0,5);
+      }
+
+      for (var item in crowd_source) {
+        if (user_checklist.contains(item) == false) {
+          Firestore.instance.collection('users').document(userId).collection('destination').document(this.widget.destination.documentID).collection('checklist').add({
+                                'title': item,
+                                'checked': false,
+                                'date': DateTime.now(),
+                                'children': []
+                              }); 
+        }
+      }
+    });
   }
 
   Future<String> createAlertDialog(BuildContext context) {
@@ -216,88 +329,7 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
       );
     });
   }
-  // Future<String> createAlertDialog(BuildContext context) {
-  //   TextEditingController myController = TextEditingController();
-
-  //   return showDialog(context: context, builder: (context) {
-  //     return AlertDialog(
-  //       title: Text("Enter Category"),
-  //       content: TextField(
-  //         controller: myController,
-  //       ),
-  //       actions: [
-  //         MaterialButton(
-  //           elevation: 5.0,
-  //           child: Text("Submit"),
-  //           onPressed: () {
-  //             Navigator.of(context).pop(myController.text.toString());
-  //           },
-  //         )
-  //       ]
-  //     );
-  //   });
-  // }
 }
-
-  
-
-
-
-
-// class EntryItem extends StatefulWidget {
-//   const EntryItem(this.entry);
-
-//   final Entry entry;
-
-//   @override
-//   _EntryItemState createState() => _EntryItemState();
-// }
-
-// class _EntryItemState extends State<EntryItem> {
-//   bool checkBoxState = false;
-
-//   Widget _buildTiles(Entry root) {
-//     if (root.children.isEmpty) return ListTile(title: Text(root.title));
-//     return ExpansionTile(
-//       key: PageStorageKey<Entry>(root),
-//       title: Text(root.title),
-//       children: root.children.map(_buildTiles).toList(),
-//       trailing: FlatButton(child: Icon(Icons.add),
-//       onPressed: () {
-//         createAlertDialog(context).then((val) {
-//           if (val != null) {
-//             setState(() {
-//               data.add(Entry(val, <Entry>[Entry('part 1')]));
-//             });
-//         }});
-//       },
-//       ),
-//       leading: 
-//       FlatButton(
-//         child:buildCheckbox(context),
-//       onPressed: () {
-//         setState(() {
-//           print(checkBoxState);
-//           checkBoxState = !checkBoxState;
-//         });
-//       },
-//       ),
-//     );
-//   }
-
-//   Widget buildCheckbox(BuildContext context) {
-//     if (checkBoxState == false) {
-//       return Icon(Icons.check_box_outline_blank);
-//     } else {
-//       return Icon(Icons.check_box);
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return _buildTiles(widget.entry);
-//   }
-// }
 
 
 
