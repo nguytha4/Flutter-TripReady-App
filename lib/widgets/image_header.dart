7fd413ed86125b3a4961cc417927b3a5b5b21be6
Shannon Farazi <shannonfarazi@gmail.com>
2020-05-25 19:08:42 -0700

    fixing activity detail screen

--- lib/widgets/image_header.dart
+++ lib/widgets/image_header.dart
@@ -14,7 +14,8 @@ class ImageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
       child: Stack(
         children: [
           Container(
@@ -37,25 +38,28 @@ class ImageHeader extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
-            bottom: 0.0,
-            child: Container(
-              color: Colors.black45,
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      label,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 35.0,
-                        letterSpacing: 1.2,
+          Visibility(
+            visible: label != null && label.length > 0,
+            child: Positioned(
+              bottom: 0.0,
+              child: Container(
+                color: Colors.black45,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        label,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35.0,
+                          letterSpacing: 1.2,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
