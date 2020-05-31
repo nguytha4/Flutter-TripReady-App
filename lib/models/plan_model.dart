d3c2ad3641b3eace3e5a145953718658a78be3fe
Shannon Farazi <shannonfarazi@gmail.com>
2020-05-31 15:21:02 -0700

    return date added

--- lib/models/plan_model.dart
+++ lib/models/plan_model.dart
@@ -5,22 +5,25 @@ class PlanModel {
   String documentID;
   String destinationID;
   DateTime travelDate;
+  DateTime returnDate;
 
   get travelDateString => DateFormat.yMMMEd().format(travelDate);
-
+  get returnDateString => DateFormat.yMMMEd().format(returnDate);
   
-  PlanModel({this.documentID, this.destinationID, this.travelDate});
+  PlanModel({this.documentID, this.destinationID, this.travelDate, this.returnDate});
 
   PlanModel.fromSnapshot(DocumentSnapshot snapshot) {
     documentID = snapshot.documentID;
     destinationID = snapshot['destinationID'];
     travelDate = (snapshot['travelDate'] as Timestamp).toDate();
+    returnDate = ((snapshot['returnDate'] ?? snapshot['travelDate']) as Timestamp).toDate();
   }
 
   Map<String, dynamic> toMap() {
     return {
       'destinationID': destinationID,
       'travelDate': travelDate,
+      'returnDate': returnDate,
     };
   }
 }
